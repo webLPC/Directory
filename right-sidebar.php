f93f4b97ba0ec6130ba237ef534563bf6c172d8e
Timothy Druley <tim.druley79@gmail.com>
2017-04-26 09:07:13 -0700

    Init

---
+++ right-sidebar.php
@@ -0,0 +1,15 @@
+<h2>Site Administrator</h2>
+
+<p><strong>Firstname Lastname</strong><br />
+Department<br />
+Title</p>
+
+<p>925.424.5555<br /><a href="mailto:email@laspositascollege.edu">email@laspositascollege.edu</a></p>
+
+<h2>Office Hours</h2>
+
+<p>Monday, Tuesday, Thursday &amp; Friday<br />
+9:30 am - 5 pm</p>
+
+<p>Wednesday<br /> 
+10 am - 6 pm</p>
